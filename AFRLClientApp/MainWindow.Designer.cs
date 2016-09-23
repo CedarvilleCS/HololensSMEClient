c81b0c312b833da910ffd8d79a6cc6365a864677
Jeffrey Todd Deane Jr <Jeffrey Todd Deane Jr>
2016-09-23 17:09:05 -0400

    Added functionality to the gui to capture and edit screenshots from the live video feed.

--- AFRLClientApp/MainWindow.Designer.cs
+++ AFRLClientApp/MainWindow.Designer.cs
@@ -28,12 +28,148 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxThumbnail1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxThumbnail2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxThumbnail3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxThumbnail4 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxThumbnail5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(640, 360);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Capture Screenshot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(675, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 360);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Send Screenshot";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxThumbnail1
+            // 
+            this.pictureBoxThumbnail1.Location = new System.Drawing.Point(665, 393);
+            this.pictureBoxThumbnail1.Name = "pictureBoxThumbnail1";
+            this.pictureBoxThumbnail1.Size = new System.Drawing.Size(128, 72);
+            this.pictureBoxThumbnail1.TabIndex = 4;
+            this.pictureBoxThumbnail1.TabStop = false;
+            this.pictureBoxThumbnail1.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBoxThumbnail2
+            // 
+            this.pictureBoxThumbnail2.Location = new System.Drawing.Point(799, 393);
+            this.pictureBoxThumbnail2.Name = "pictureBoxThumbnail2";
+            this.pictureBoxThumbnail2.Size = new System.Drawing.Size(128, 72);
+            this.pictureBoxThumbnail2.TabIndex = 5;
+            this.pictureBoxThumbnail2.TabStop = false;
+            this.pictureBoxThumbnail2.Click += new System.EventHandler(this.pictureBoxThumbnail2_Click);
+            // 
+            // pictureBoxThumbnail3
+            // 
+            this.pictureBoxThumbnail3.Location = new System.Drawing.Point(933, 393);
+            this.pictureBoxThumbnail3.Name = "pictureBoxThumbnail3";
+            this.pictureBoxThumbnail3.Size = new System.Drawing.Size(128, 72);
+            this.pictureBoxThumbnail3.TabIndex = 6;
+            this.pictureBoxThumbnail3.TabStop = false;
+            this.pictureBoxThumbnail3.Click += new System.EventHandler(this.pictureBoxThumbnail3_Click);
+            // 
+            // pictureBoxThumbnail4
+            // 
+            this.pictureBoxThumbnail4.Location = new System.Drawing.Point(1067, 393);
+            this.pictureBoxThumbnail4.Name = "pictureBoxThumbnail4";
+            this.pictureBoxThumbnail4.Size = new System.Drawing.Size(128, 72);
+            this.pictureBoxThumbnail4.TabIndex = 7;
+            this.pictureBoxThumbnail4.TabStop = false;
+            this.pictureBoxThumbnail4.Click += new System.EventHandler(this.pictureBoxThumbnail4_Click);
+            // 
+            // pictureBoxThumbnail5
+            // 
+            this.pictureBoxThumbnail5.Location = new System.Drawing.Point(1201, 393);
+            this.pictureBoxThumbnail5.Name = "pictureBoxThumbnail5";
+            this.pictureBoxThumbnail5.Size = new System.Drawing.Size(128, 72);
+            this.pictureBoxThumbnail5.TabIndex = 8;
+            this.pictureBoxThumbnail5.TabStop = false;
+            this.pictureBoxThumbnail5.Click += new System.EventHandler(this.pictureBoxThumbnail5_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1351, 478);
+            this.Controls.Add(this.pictureBoxThumbnail5);
+            this.Controls.Add(this.pictureBoxThumbnail4);
+            this.Controls.Add(this.pictureBoxThumbnail3);
+            this.Controls.Add(this.pictureBoxThumbnail2);
+            this.Controls.Add(this.pictureBoxThumbnail1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "MainWindow";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxThumbnail5)).EndInit();
+            this.ResumeLayout(false);
+
     }
 
-    #endregion
-  }
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail1;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail2;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail3;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail4;
+        private System.Windows.Forms.PictureBox pictureBoxThumbnail5;
+    }
 }
 
--- AFRLClientApp/MainWindow.cs
+++ AFRLClientApp/MainWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,152 @@ namespace AFRLClientApp
 {
   public partial class MainWindow : Form
   {
-    public MainWindow()
-    {
-      InitializeComponent();
+        Bitmap activeBMP;
+
+        int numPics = 0;
+
+        List<Bitmap> imageHistoryThumbnails = new List<Bitmap>();
+
+        List<PictureBox> pictureBoxThumbnails = new List<PictureBox>();
+
+        private Pen _pen;
+
+        private bool _draw = false;
+
+        private int _x = -1;
+
+        private int _y = -1;
+
+        public MainWindow()
+        {
+          InitializeComponent();
+          _pen = new Pen(Color.Black, 5);
+          _pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+          _pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            pictureBoxThumbnails.Add(pictureBoxThumbnail1);
+            pictureBoxThumbnails.Add(pictureBoxThumbnail2);
+            pictureBoxThumbnails.Add(pictureBoxThumbnail3);
+            pictureBoxThumbnails.Add(pictureBoxThumbnail4);
+            pictureBoxThumbnails.Add(pictureBoxThumbnail5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new bitmap.
+            Bitmap bmpScreenshot = new Bitmap(640, 360, PixelFormat.Format32bppArgb);
+
+            // Create a graphics object from the bitmap.
+            Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            // Take the screenshot according to the positioning of the video player.
+            gfxScreenshot.CopyFromScreen(axWindowsMediaPlayer1.PointToScreen(new System.Drawing.Point()).X,
+                                        axWindowsMediaPlayer1.PointToScreen(new System.Drawing.Point()).Y,
+                                        0, 0, axWindowsMediaPlayer1.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+            
+            // Print out the screenshot to the picture box.
+            pictureBox1.Image = bmpScreenshot;
+            pictureBox1.Height = bmpScreenshot.Height;
+            pictureBox1.Width = bmpScreenshot.Width;
+
+            imageHistoryThumbnails.Insert(0, bmpScreenshot);
+            UpdateThumbnails();
+            activeBMP = bmpScreenshot;
+        }
+
+        private void UpdateThumbnails()
+        {
+            int numThumbnails = 5;
+
+            if (imageHistoryThumbnails.Count < 5)
+            {
+                numThumbnails = imageHistoryThumbnails.Count;
+            }
+
+            for(int i = 0; i < numThumbnails; i++)
+            {
+                pictureBoxThumbnails[i].Image = new Bitmap(imageHistoryThumbnails[i], 128, 72);
+            }
+        }
+
+
+        Color color = Color.Red;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+           _draw = true;
+
+            Graphics drawPanel = Graphics.FromImage(activeBMP);
+
+            _pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            _pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            drawPanel.DrawRectangle(_pen, e.X, e.Y, 2, 2);
+            drawPanel.Save();
+            pictureBox1.Image = activeBMP;
+ 
+            _x = e.X;
+            _y = e.Y;
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_draw)
+            {
+                Graphics g = Graphics.FromImage(activeBMP);
+                SolidBrush brush = new SolidBrush(color);
+                g.FillRectangle(brush, e.X, e.Y, 2, 2);
+                g.Save();
+                pictureBox1.Image = activeBMP;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _draw = false;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageHistoryThumbnails[0];
+            activeBMP = imageHistoryThumbnails[0];
+        }
+
+        private void pictureBoxThumbnail2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageHistoryThumbnails[1];
+            activeBMP = imageHistoryThumbnails[1];
+        }
+
+        private void pictureBoxThumbnail3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageHistoryThumbnails[2];
+            activeBMP = imageHistoryThumbnails[2];
+        }
+
+        private void pictureBoxThumbnail4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageHistoryThumbnails[3];
+            activeBMP = imageHistoryThumbnails[3];
+        }
+
+        private void pictureBoxThumbnail5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageHistoryThumbnails[4];
+            activeBMP = imageHistoryThumbnails[4];
+        }
     }
-  }
 }
