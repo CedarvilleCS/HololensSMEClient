8841934f2ad355debf98fc1de523601b7738617b
Kidron Ray Filbrun <Kidron Ray Filbrun>
2017-01-13 14:35:19 -0500

    New version of the client

---
+++ ARTAP-Client/ARTAP-Client/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace ARTAPclient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AsynchronousSocketListener _listener;
+
+        private string _ip;
+
+        private string _userName;
+
+        private string _password;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            buttonConnect.IsEnabled = false;
+            buttonConnect.Content = "Connecting...";
+            _ip = textBoxIP.Text;
+            string port = textBoxPort.Text;
+            _userName = textBoxUserName.Text;
+            _password = passwordBoxPassword.Password;
+
+            if (ValidateText(_ip, "IP") && ValidateText(port, "port") &&
+                ValidateText(_userName, "user name") && ValidateText(_password, "password"))
+            {
+                IPEndPoint hostEndPoint;
+                if(!TryGetIPEndPoint(_ip, port, out hostEndPoint))
+                {
+                    return;
+                }
+
+                _listener = new AsynchronousSocketListener(hostEndPoint);
+                _listener.ConnectionEstablished += Listener_ConnectionEstablished;
+                _listener.ConnectionTimedOut += Listener_ConnectionTimedOut;
+                _listener.Connect();
+            }
+        }
+
+        private void Listener_ConnectionEstablished(object sender, EventArgs e)
+        {
+            _listener.ConnectionEstablished -= Listener_ConnectionEstablished;
+            _listener.ConnectionTimedOut -= Listener_ConnectionTimedOut;
+
+            ///
+            /// Needed for cross-thread window launch
+            ///
+            this.Dispatcher.BeginInvoke((Action) (() =>
+            {
+                VideoStreamWindow video = new ARTAPclient.VideoStreamWindow(_ip, _userName, _password);
+                video.Show();
+
+                ScreenshotAnnotationsWindow annotations = new ARTAPclient.ScreenshotAnnotationsWindow(video, _listener);
+                annotations.Show();
+                this.Close();
+            }));
+        }
+
+        private void Listener_ConnectionTimedOut(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke((Action)(() =>
+            {
+                buttonConnect.Content = "Connect";
+                buttonConnect.IsEnabled = true;
+                MessageBox.Show("Connection timed out, please verify IP and Port.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }));
+        }
+
+        private bool TryGetIPEndPoint(string ip, string port, out IPEndPoint endPoint)
+        {
+            endPoint = null;
+            IPAddress hostAddr;
+            if (!IPAddress.TryParse(ip, out hostAddr))
+            {
+                MessageBox.Show("Please provide a valid IP.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            int portNum;
+            if (!int.TryParse(port, out portNum))
+            {
+                MessageBox.Show("Please provide a valid port.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            endPoint = new System.Net.IPEndPoint(hostAddr, portNum);
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if string is null or empty, launches window if it is
+        /// </summary>
+        /// <param name="input">The string to be tested</param>
+        /// <param name="name">Name of the field</param>
+        /// <returns></returns>
+        private bool ValidateText(string input, string name)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                MessageBox.Show("Please provide input for " + name + ".", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ ARTAP-Client/ARTAP-Client/VideoStreamWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace ARTAPclient
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class VideoStreamWindow : Window
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// Aspect ratio of screen
+        /// </summary>
+        private double _aspectRatio;
+
+        /// <summary>
+        /// Is the height being adjusted
+        /// </summary>
+        private bool? _adjustingHeight = null;
+
+        private string _connectionURL;
+
+        #endregion
+
+        #region Constuctor
+
+        public VideoStreamWindow(string ip, string user, string password)
+        {
+            InitializeComponent();
+            this.SourceInitialized += Window_SourceInitialized;
+
+            _connectionURL = String.Format("http://{0}:{1}@{2}/api/holographic/stream/live_low.mp4?holo=false&pv=true&mic=false&loopback=false",
+                user, password, ip);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public BitmapImage CaptureScreen()
+        {
+            Bitmap bmp = videoControl.GetCurrentFrame();
+            return ConvertBitmap(bmp);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Converts System.Drawing.Bitmap to BitmapImage
+        /// Borrowed from: http://stackoverflow.com/questions/26260654/wpf-converting-bitmap-to-imagesource
+        /// </summary>
+        /// <param name="src">Source bitmap</param>
+        /// <returns>Converted BitmapImage</returns>
+        public BitmapImage ConvertBitmap(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            videoControl.StartPlay(new Uri(_connectionURL), TimeSpan.FromSeconds(15));
+        }
+
+        private void videoControlStreamFailed(object sender, WebEye.StreamFailedEventArgs e)
+        {
+            if (e.RoutedEvent.Name == "StreamFailed")
+            {
+                MessageBox.Show(
+                    ((WebEye.StreamFailedEventArgs)e).Error,
+                    "Stream Player Demo",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
+
+        #region Fixed Aspect Ratio Components
+
+        ///
+        /// Code used for fixed aspect ratio borrowed from here:
+        /// http://stackoverflow.com/questions/2471867/resize-a-wpf-window-but-maintain-proportions
+        ///
+
+        internal enum SWP
+        {
+            NOMOVE = 0x0002
+        }
+        internal enum WM
+        {
+            WINDOWPOSCHANGING = 0x0046,
+            EXITSIZEMOVE = 0x0232,
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WINDOWPOS
+        {
+            public IntPtr hwnd;
+            public IntPtr hwndInsertAfter;
+            public int x;
+            public int y;
+            public int cx;
+            public int cy;
+            public int flags;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        public static System.Windows.Point GetMousePosition() // mouse position relative to screen
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new System.Windows.Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs ea)
+        {
+            HwndSource hwndSource = (HwndSource)HwndSource.FromVisual((Window)sender);
+            hwndSource.AddHook(DragHook);
+
+            _aspectRatio = this.Width / this.Height;
+        }
+
+        private IntPtr DragHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch ((WM)msg)
+            {
+                case WM.WINDOWPOSCHANGING:
+                    {
+                        WINDOWPOS pos = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+
+                        if ((pos.flags & (int)SWP.NOMOVE) != 0)
+                            return IntPtr.Zero;
+
+                        Window wnd = (Window)HwndSource.FromHwnd(hwnd).RootVisual;
+                        if (wnd == null)
+                            return IntPtr.Zero;
+
+                        // determine what dimension is changed by detecting the mouse position relative to the 
+                        // window bounds. if gripped in the corner, either will work.
+                        if (!_adjustingHeight.HasValue)
+                        {
+                            System.Windows.Point p = GetMousePosition();
+
+                            double diffWidth = Math.Min(Math.Abs(p.X - pos.x), Math.Abs(p.X - pos.x - pos.cx));
+                            double diffHeight = Math.Min(Math.Abs(p.Y - pos.y), Math.Abs(p.Y - pos.y - pos.cy));
+
+                            _adjustingHeight = diffHeight > diffWidth;
+                        }
+
+                        if (_adjustingHeight.Value)
+                            pos.cy = (int)(pos.cx / _aspectRatio); // adjusting height to width change
+                        else
+                            pos.cx = (int)(pos.cy * _aspectRatio); // adjusting width to heigth change
+
+                        Marshal.StructureToPtr(pos, lParam, true);
+                        handled = true;
+                    }
+                    break;
+                case WM.EXITSIZEMOVE:
+                    _adjustingHeight = null; // reset adjustment dimension and detect again next time window is resized
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        #endregion
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var windows = Application.Current.Windows;
+            foreach (var item in windows)
+            {
+                (item as Window).Close();
+            }
+            Application.Current.Shutdown();
+        }
+    }
+}
