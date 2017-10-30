711aba591aef86007cfb8c5c217b0231e86ecd2e
tfroberg <tfroberg@cedarville.edu>
2017-10-30 15:56:59 -0400

    Add to task model

--- ARTAP-Client/ARTAP-Client/TaskList.cs
+++ ARTAP-Client/ARTAP-Client/TaskList.cs
@@ -6,6 +6,7 @@ namespace WpfApplication1
     public class TaskList
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public List<Task> Tasks { get; set; }
         public bool IsCompleted => !Tasks.AsParallel().Any(task => !task.IsCompleted);
     }
