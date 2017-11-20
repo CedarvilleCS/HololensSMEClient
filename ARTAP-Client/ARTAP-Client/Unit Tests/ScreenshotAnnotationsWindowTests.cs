625cfa2dd8a3408eb9e77b859d9dbd43c4b3d28a
tfroberg <tfroberg@cedarville.edu>
2017-11-20 15:17:27 -0500

    Start on tests

--- ARTAP-Client/ARTAP-Client/Unit Tests/ScreenshotAnnotationsWindowTests.cs
+++ ARTAP-Client/ARTAP-Client/Unit Tests/ScreenshotAnnotationsWindowTests.cs
@@ -1,18 +1,19 @@
-﻿
-
+﻿using Moq;
 using Xunit;
 
 namespace WpfApplication1.Unit_Tests
 {
     public class ScreenshotAnnotationsWindowTests
     {
+        private Mock<ScreenshotAnnotationsWindowTests> _window;
+
         public ScreenshotAnnotationsWindowTests()
         {
-
+            //_window.
         }
 
         [Fact]
-        public void test()
+        public void Constructor()
         {
 
         }
